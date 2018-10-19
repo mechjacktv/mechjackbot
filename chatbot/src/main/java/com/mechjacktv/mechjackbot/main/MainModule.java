e25552dd1ec4e5fa13bc8c403df0acb4dd5fbce3
mechjacktv <mechjack@laptop-code.domain>
2018-10-19 17:15:23 -0500

    Binding configuration is important

--- chatbot/src/main/java/com/mechjacktv/mechjackbot/main/MainModule.java
+++ chatbot/src/main/java/com/mechjacktv/mechjackbot/main/MainModule.java
@@ -7,12 +7,16 @@ import com.mechjacktv.mechjackbot.command.interceptor.CommandInterceptorsModule;
 import com.mechjacktv.mechjackbot.chatbot.PircBotXChatBot;
 import com.mechjacktv.mechjackbot.chatbot.PircBotXListener;
 import com.mechjacktv.mechjackbot.chatbot.PircBotXMessageEventHandler;
+import com.mechjacktv.mechjackbot.configuration.DefaultBotConfiguration;
+import com.mechjacktv.mechjackbot.configuration.PropertiesAppConfiguration;
 import com.mechjacktv.util.UtilsModule;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.pircbotx.hooks.Listener;
 
 class MainModule extends AbstractModule {
+
+    private static final String MAP_DB_LOCATION = System.getProperty("user.home") + "/.mechjackbot.db";
 
     @Override
     protected final void configure() {
@@ -21,11 +25,11 @@ class MainModule extends AbstractModule {
         install(new UtilsModule());
 
         // Bind MapDB
-        bind(DB.class).toInstance(DBMaker.memoryDB().make());
+        bind(DB.class).toInstance(DBMaker.fileDB(MAP_DB_LOCATION).make());
 
         // Bind ChatBot
-        bind(AppConfiguration.class).asEagerSingleton();
-        bind(BotConfiguration.class).asEagerSingleton();
+        bind(AppConfiguration.class).to(PropertiesAppConfiguration.class).asEagerSingleton();
+        bind(BotConfiguration.class).to(DefaultBotConfiguration.class).asEagerSingleton();
         bind(ChatBot.class).to(PircBotXChatBot.class).asEagerSingleton();
         bind(Listener.class).to(PircBotXListener.class).asEagerSingleton();
         bind(MessageEventHandler.class).to(PircBotXMessageEventHandler.class).asEagerSingleton();
