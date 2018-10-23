d1ec18e969d8dc9d0594bbe7295ec5dfdd9fd9ab
mechjacktv <mechjack@laptop-code.domain>
2018-10-23 17:16:40 -0500

    Add TimeUtils contract tests and exexute them on DefaultTimeUtils

--- chatbot/src/main/java/com/mechjacktv/util/DefaultTimeUtils.java
+++ chatbot/src/main/java/com/mechjacktv/util/DefaultTimeUtils.java
@@ -1,9 +1,12 @@
 package com.mechjacktv.util;
 
+import java.util.Objects;
+
 final class DefaultTimeUtils implements TimeUtils {
 
   @Override
-  public final Integer secondsAsMs(final int seconds) {
+  public final Integer secondsAsMs(final Integer seconds) {
+    Objects.requireNonNull(seconds, "`seconds` **MUST** not be `null`");
     return seconds * SECOND;
   }
 
