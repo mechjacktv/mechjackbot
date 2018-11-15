4d1d7b28884ccefb078f5110632b6f247096750c
MechJack TV <mechjacktv@gmail.com>
2018-11-14 19:24:20 -0600

    Simplified softenException implementation

--- src/main/java/com/mechjacktv/util/DefaultExecutionUtils.java
+++ src/main/java/com/mechjacktv/util/DefaultExecutionUtils.java
@@ -1,6 +1,7 @@
 package com.mechjacktv.util;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +19,8 @@ public final class DefaultExecutionUtils implements ExecutionUtils {
   }
 
   @Override
-  public final void softenException(RunnableWithException runnable, Class<? extends RuntimeException> exceptionClass) {
+  public final void softenException(final RunnableWithException runnable,
+      final Class<? extends RuntimeException> exceptionClass) {
     this.softenException(() -> {
       runnable.run();
       return null;
@@ -26,28 +28,21 @@ public final class DefaultExecutionUtils implements ExecutionUtils {
   }
 
   @Override
-  public final <T> T softenException(SupplierWithException<T> supplier,
-      Class<? extends RuntimeException> exceptionClass) {
+  public final <T> T softenException(final SupplierWithException<T> supplier,
+      final Class<? extends RuntimeException> exceptionClass) {
     try {
       return supplier.get();
-    } catch (final RuntimeException e) {
-      throw e;
     } catch (final Exception e1) {
+      if (RuntimeException.class.isAssignableFrom(e1.getClass())) {
+        throw (RuntimeException) e1;
+      }
       try {
-        try {
-          final Constructor<? extends RuntimeException> constructor = exceptionClass
-              .getConstructor(String.class, Throwable.class);
+        final Constructor<? extends RuntimeException> constructor = exceptionClass
+            .getConstructor(String.class, Throwable.class);
 
-          throw constructor.newInstance(e1.getMessage(), e1);
-        } catch (final NoSuchMethodException e2) {
-          final Constructor<? extends RuntimeException> constructor = exceptionClass
-              .getConstructor(Throwable.class);
-
-          throw constructor.newInstance(e1);
-        }
-      } catch (final RuntimeException e2) {
-        throw e2;
-      } catch (final Exception e2) {
+        throw constructor.newInstance(e1.getMessage(), e1);
+      } catch (final NoSuchMethodException | InstantiationException | IllegalAccessException
+          | IllegalArgumentException | InvocationTargetException e2) {
         log.error(String.format("Failed to instantiate RuntimeException. exceptionClass=%s, ",
             exceptionClass.getCanonicalName()), e2);
         throw new SoftenedException(e1.getMessage(), e1);
