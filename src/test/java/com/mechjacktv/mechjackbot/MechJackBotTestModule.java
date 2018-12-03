0f9ac6c0f5eae39bc43df4544a5a3055bd7f0303
MechJack TV <mechjacktv@gmail.com>
2018-12-03 01:53:02 -0600

    ISSUE-13: Fix type bindings

--- src/test/java/com/mechjacktv/mechjackbot/MechJackBotTestModule.java
+++ src/test/java/com/mechjacktv/mechjackbot/MechJackBotTestModule.java
@@ -1,16 +1,13 @@
 package com.mechjacktv.mechjackbot;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
-
-import com.mechjacktv.mechjackbot.command.DefaultCommandUtils;
 
 public class MechJackBotTestModule extends AbstractModule {
 
   @Override
   protected void configure() {
-    this.bind(CommandUtils.class).to(DefaultCommandUtils.class).in(Scopes.SINGLETON);
     this.bind(TestCommand.class);
+    this.bind(Command.class).to(TestCommand.class);
     this.bind(TestMessageEvent.class);
     this.bind(MessageEvent.class).to(TestMessageEvent.class);
   }
