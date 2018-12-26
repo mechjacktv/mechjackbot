755496b43c1ed7ebef7d1f58682869452ce0367c
MechJack TV <mechjacktv@gmail.com>
2018-12-25 23:48:36 -0600

    ISSUE-3: Marked CommandBody final

--- src/main/java/com/mechjacktv/mechjackbot/command/custom/CommandBody.java
+++ src/main/java/com/mechjacktv/mechjackbot/command/custom/CommandBody.java
@@ -2,7 +2,7 @@ package com.mechjacktv.mechjackbot.command.custom;
 
 import com.mechjacktv.util.typedobject.TypedString;
 
-public class CommandBody extends TypedString {
+public final class CommandBody extends TypedString {
 
   public static CommandBody of(final String value) {
     return TypedString.of(CommandBody.class, value);
